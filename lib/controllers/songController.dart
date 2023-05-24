574dbaa037adb6223aadef2490069f09cb16ed5b
Victor Tse <victor.tse@bluepin.hk>
2023-05-24 15:25:55 +0800

    audio player (tbc)

--- lib/controllers/songController.dart
+++ lib/controllers/songController.dart
@@ -2,18 +2,48 @@ import 'dart:convert';
 
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:just_audio/just_audio.dart';
 
 class SongController extends GetxController {
   var isLoading = true.obs;
   var responseJson = {}.obs;
+
+  var isPlaying = false.obs;
+  var audioPlayer = AudioPlayer();
+
+  @override
+  void onInit() {
+    super.onInit();
+    audioPlayer.playerStateStream.listen((state) {
+      if (state.playing) {
+        isPlaying(true);
+      } else {
+        isPlaying(false);
+      }
+    });
+  }
 
   void fetchSongs(String term) async {
     try {
       isLoading(true);
       var response = await http.get(Uri.parse('https://itunes.apple.com/search?term=$term'));
       responseJson.value = json.decode(response.body.toString());
+      responseJson['isPlaying'] = false;
     } finally {
       isLoading(false);
     }
+  }
+
+  void playSong(String url) async {
+    await audioPlayer.setUrl(url);
+    await audioPlayer.play();
+  }
+
+  void pauseSong() async {
+    await audioPlayer.pause();
+  }
+
+  void stopSong() async {
+    await audioPlayer.stop();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -89,78 +89,84 @@ class _MyHomePageState extends State<MyHomePage> {
                             child: Text('Sorry, no song found.'));
                       }
                       List<Widget> widgets = songs
-                          .map((song) => Card(
-                              elevation: 2,
-                              color: Colors.white,
-                              margin: const EdgeInsets.all(8.0),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(3),
-                              ),
-                              child: Row(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.fromLTRB(20.0, 25.0, 20.0, 25.0),
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(5.0),
-                                      child: SizedBox(
-                                        width: 80,
-                                        height: 80,
-                                        child: song.artworkUrl100 != null
-                                            ? Image.network(
-                                          song.artworkUrl100!,
-                                          fit: BoxFit.cover,
-                                          errorBuilder: (_, __, ___) => Image.asset(
+                          .map((song) => GestureDetector(
+                        onTap: () {
+                          songController.isPlaying.value ? songController.pauseSong()
+                           : songController.playSong(song.previewUrl!);
+                        },
+                            child: Card(
+                                elevation: 2,
+                                color: Colors.white,
+                                margin: const EdgeInsets.all(8.0),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(3),
+                                ),
+                                child: Row(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.fromLTRB(20.0, 25.0, 20.0, 25.0),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(5.0),
+                                        child: SizedBox(
+                                          width: 80,
+                                          height: 80,
+                                          child: song.artworkUrl100 != null
+                                              ? Image.network(
+                                            song.artworkUrl100!,
+                                            fit: BoxFit.cover,
+                                            errorBuilder: (_, __, ___) => Image.asset(
+                                              'assets/no_artwork_available.png',
+                                              fit: BoxFit.cover,
+                                            ),
+                                          )
+                                              : Image.asset(
                                             'assets/no_artwork_available.png',
                                             fit: BoxFit.cover,
                                           ),
-                                        )
-                                            : Image.asset(
-                                          'assets/no_artwork_available.png',
-                                          fit: BoxFit.cover,
                                         ),
                                       ),
                                     ),
-                                  ),
-                                  Expanded(
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(vertical: 15.0),
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            song.trackName ?? 'Unknown Track',
-                                            style: Theme.of(context).textTheme.subtitle1,
-                                            textScaleFactor: 1.1,
-                                          ),
-                                          const SizedBox(
-                                            height: 5,
-                                          ),
-                                          Text(song.artistName ?? 'Unknown Artist',
-                                              style: Theme.of(context).textTheme.bodyText2),
-                                          const SizedBox(
-                                            height: 5,
-                                          ),
-                                          Text(song.collectionName ?? 'Unknown Album',
-                                              style: Theme.of(context).textTheme.caption),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: const [
-                                      SizedBox(width: 20.0),
-                                      Padding(
-                                        padding: EdgeInsets.only(right: 20.0),
-                                        child: Icon(
-                                          Icons.play_arrow,
-                                          color: Colors.black,
+                                    Expanded(
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(vertical: 15.0),
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              song.trackName ?? 'Unknown Track',
+                                              style: Theme.of(context).textTheme.subtitle1,
+                                              textScaleFactor: 1.1,
+                                            ),
+                                            const SizedBox(
+                                              height: 5,
+                                            ),
+                                            Text(song.artistName ?? 'Unknown Artist',
+                                                style: Theme.of(context).textTheme.bodyText2),
+                                            const SizedBox(
+                                              height: 5,
+                                            ),
+                                            Text(song.collectionName ?? 'Unknown Album',
+                                                style: Theme.of(context).textTheme.caption),
+                                          ],
                                         ),
                                       ),
-                                    ],
-                                  ),
-                                ],
-                              )))
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const SizedBox(width: 20.0),
+                                        Padding(
+                                          padding: const EdgeInsets.only(right: 20.0),
+                                          child: Icon(
+                                            songController.isPlaying.value ? Icons.play_arrow : Icons.pause,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )),
+                          ))
                           .toList();
                       return SingleChildScrollView(
                           child: Column(children: widgets));
